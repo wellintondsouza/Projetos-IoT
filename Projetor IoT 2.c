3a33fea9f1169445630f788e718ec17af15ee3a6
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2024-09-12 08:53:54 -0300

    Update Projetor IoT 2.c

--- Projetor IoT 2.c
+++ Projetor IoT 2.c
@@ -1,32 +1,29 @@
-// C++ code for Arduino
 
-// Define the trigger and echo pins
+
 const int triggerPin = 7;
 const int echoPin = 6;
-const int ledPin1 = 9; // Primeiro LED na porta 9
-const int ledPin2 = 8; // Segundo LED na porta 8
+const int ledPin1 = 9; 
+const int ledPin2 = 8; 
 
 void setup() {
-    pinMode(triggerPin, OUTPUT); // Set trigger pin as output
-    pinMode(echoPin, INPUT);     // Set echo pin as input
-    pinMode(ledPin1, OUTPUT);    // Set primeiro LED pin as output
-    pinMode(ledPin2, OUTPUT);    // Set segundo LED pin as output
-    Serial.begin(9600);          // Initialize serial communication for debugging
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);     
+    pinMode(ledPin1, OUTPUT);   
+    pinMode(ledPin2, OUTPUT);   
+    Serial.begin(9600);        
 }
 
 long readUltrasonicDistance() {
-    // Send a 10-microsecond pulse to trigger the sensor
+
     digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
     digitalWrite(triggerPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(triggerPin, LOW);
 
-    // Read the echo pulse duration
     long duration = pulseIn(echoPin, HIGH);
 
-    // Calculate the distance in cm
-    long distance = duration * 0.0344 / 2; // Speed of sound: 0.0344 cm/µs
+    long distance = duration * 0.0344 / 2; 
     return distance;
 }
 
@@ -36,19 +33,17 @@ void loop() {
     Serial.print(distancia);
     Serial.println(" cm");
 
-    // Control the first LED based on distance
     if (distancia < 180) {
         digitalWrite(ledPin1, HIGH);
     } else {
         digitalWrite(ledPin1, LOW);
     }
 
-    // Control the second LED based on distance
-    if (distancia < 90) { // Ajuste o valor conforme necessário para o segundo LED
+    if (distancia < 90) { 
         digitalWrite(ledPin2, HIGH);
     } else {
         digitalWrite(ledPin2, LOW);
     }
 
-    delay(100); // Delay a bit to improve performance
+    delay(100);
 }
